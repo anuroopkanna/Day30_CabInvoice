3aa60b1f465f2363c3395013ad13270e6bfe6ba4
anuroopkanna <anuroopindia16@gmail.com>
2023-04-11 20:59:41 +0530

    InvoiceOfCab

---
+++ main/java/com/BridgeLabs/CabInvoice.java
@@ -0,0 +1,11 @@
+package com.BridgeLabs;
+
+public class CabInvoice {
+    public static final int COST_PER_KM=10;
+    public static final int COST_PER_MIN=1;
+    public static final int MIN_FARE=5;
+    public Double calculateFare(double distance,double time){
+        double fare = distance*COST_PER_KM+COST_PER_MIN;
+        return fare<MIN_FARE?MIN_FARE:fare;
+    }
+}
